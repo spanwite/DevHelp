0e0c474c03d28271168cd8963a19de2fab2f467e
spanwite <spanwite@ya.ru>
2026-04-17 20:22:49 +0300

    chore: Plug in Prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="container mx-auto">
-      <div className="text-center font-bold text-3xl text-violet-500 mt-10">
+    <div className='container mx-auto'>
+      <div className='mt-10 text-center text-3xl font-bold text-violet-500'>
         Hello from /home
       </div>
     </div>

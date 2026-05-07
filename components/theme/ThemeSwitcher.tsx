075f587cb71a447514a31ec61d8859313ea29931
spanwite <spanwite@ya.ru>
2026-05-07 20:10:35 +0300

    refactor(theme): Remove unnecessary classes in switcher

--- components/theme/ThemeSwitcher.tsx
+++ components/theme/ThemeSwitcher.tsx
@@ -18,8 +18,8 @@ export function ThemeSwitcher() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant='outline' size='icon'>
-          <Sun className='h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90' />
-          <Moon className='absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0' />
+          <Sun className='size-4.5 dark:hidden' />
+          <Moon className='size-4.5 hidden dark:block' />
           <span className='sr-only'>Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>

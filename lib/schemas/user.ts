a250ecebac6eed75882278aa5f1ef35854dee387
spanwite <spanwite@ya.ru>
2026-05-14 11:47:48 +0300

    Add proper password validation in user schema

--- lib/schemas/user.ts
+++ lib/schemas/user.ts
@@ -16,8 +16,15 @@ export const UserSchema = z.object({
 
   password: z
     .string()
-    .min(6, 'Password must be at least 6 characters.')
-    .max(100, 'Password cannot be more than 100 characters.'),
+    .min(8, 'Password must be at least 8 characters.')
+    .max(100, 'Password cannot be more than 100 characters.')
+    .regex(/[a-z]/, 'Password must contain at least one lowercase letter.')
+    .regex(/[A-Z]/, 'Password must contain at least one uppercase letter.')
+    .regex(/[0-9]/, 'Password must contain at least one number.')
+    .regex(
+      /[!@#$%^&*(),.?":{}|<>_]/,
+      'Password must contain at least one special character.'
+    ),
 
   avatar: z.string().url('Avatar must be a valid URL.').optional(),
 
